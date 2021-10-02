66a1d996d6c04ec1a2bca1b1e57295cb91fdefcd
yousefahmed <yousefa.mohamed12@gmail.com>
2021-10-02 21:23:19 +0200

    Revert "correct path name"
    This reverts commit 18c1c3949466b59962104aa3fd7b82ff3ba8b630.

--- products/models.py
+++ products/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class product(models.Model):
-    name = models.CharField(max_length=50)
-    content = models.TextField()
-    price = models.DecimalField(max_digits=10,decimal_places=2)
-    image = models.ImageField(upload_to='photos/%y/%m/%d')
-    available = models.BooleanField(default=False)
